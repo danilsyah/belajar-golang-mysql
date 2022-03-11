bf7e29080072eaf1064b39f00f6f0b01ffc3f8d2
danilsyah <danilsyaharihardjo@gmail.com>
2022-03-11 22:05:43 +0700

    open database

--- database_test.go
+++ database_test.go
@@ -1,6 +1,7 @@
 package belajargolangmysql
 
 import (
+	"database/sql"
 	"testing"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -8,4 +9,16 @@ import (
 
 func TestEmpty(t *testing.T) {
 
+}
+
+func TestOpenConnection(t *testing.T) {
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/latihangolang")
+
+	if err != nil {
+		panic(err)
+	}
+
+	// gunakan query db
+
+	db.Close()
 }
